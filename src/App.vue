d7887a86f0313bcf82d1537a7a7e7781a294357b
adrianobarboza31 <barbozaa022@gmail.com>
2022-11-14 15:46:54 +0100

    add header component

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <HeaderComponent/>
+ <main>
+  
+ </main>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue'
+import NavBar from './components/NavBar.vue'
+import CardsComponent from './components/CardsComponent.vue'
+  export default {
+    data(){
+      return{
+        apiURL:"https://www.breakingbadapi.com/api/characters"
+      }
+    },
+    components:{
+      HeaderComponent,
+      NavBar,
+      CardsComponent
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1 class="ps-2 pt-2">Breaking Bad Api</h1>
+  <div class="container">
+    <div class="dropdown pt-4">
+      <button class="btn bg-white dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Select category
+      </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+      <button class="dropdown-item" type="button">Action</button>
+      <button class="dropdown-item" type="button">Another action</button>
+      <button class="dropdown-item" type="button">Something else here</button>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+h1{
+  color: white;
+}
+</style>
