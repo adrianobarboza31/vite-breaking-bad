691cb095741f8841e98daf9df743cf67876ce6d8
adrianobarboza31 <barbozaa022@gmail.com>
2022-11-14 17:24:26 +0100

    add cardcomponent

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,41 @@
 <template>
   <HeaderComponent/>
  <main>
-  
+  <CardsComponent :Charachter="charachterList"/>
  </main>
 </template>
 
 <script>
+import axios from 'axios';
 import HeaderComponent from './components/HeaderComponent.vue'
 import NavBar from './components/NavBar.vue'
 import CardsComponent from './components/CardsComponent.vue'
   export default {
     data(){
       return{
-        apiURL:"https://www.breakingbadapi.com/api/characters"
+        apiURL:"https://www.breakingbadapi.com/api/characters",
+        charachterList:[]
       }
     },
     components:{
       HeaderComponent,
       NavBar,
       CardsComponent
+    },
+    methods:{
+      getCharachters(){
+        axios.get(this.apiURL).then(
+          (res)=>{
+            this.charachterList=[...res.data];
+            console.log(this.charachterList)
+          },
+          )
+      }
+    },
+    created(){
+      this.getCharachters()
     }
+    // "char_id":1,"name":"Walter White","birthday":"09-07-1958","occupation":["High School Chemistry Teacher","Meth King Pin"],"img":"https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg","status":"Presumed dead","nickname":"Heisenberg","appearance":[1,2,3,4,5],"portrayed":"Bryan Cranston","category":"Breaking Bad","better_call_saul_appearance":[]}
   }
 </script>
 
--- src/components/CardsComponent.vue
+++ src/components/CardsComponent.vue
@@ -1,15 +1,51 @@
 <template>
-  <div>
-
+  <div class="container bg-white mt-4 pt-5">
+<div class="found pt-3 pb-3 ps-3">
+  <strong>Found 62 characters</strong>
+</div>
+<div class="d-flex row  cc">
+  <div class="ab-col-5 pt-3"  v-for='(item,index) in  Charachter'>
+    <div>
+      <img :src="item.img" alt="">
+    </div>
+    <p><strong>{{item.name}}</strong></p>
+    <p>{{item.status}}</p>
+    <p>{{item.category}}</p>
+  </div>
+</div>
   </div>
 </template>
 
 <script>
   export default {
-    
+    props:['Charachter']
   }
 </script>
 
 <style lang="scss" scoped>
-
+@use '../assets/style/partials/variablse.scss' as *;
+  .found{
+    background: $secondarycolor;
+    width: 95%;
+    margin: auto;
+    color: white;
+  }
+  .cc{
+    width: 95%;
+   margin: auto;
+  text-align: center;
+  }
+  .ab-col-5{
+    width: calc((100% - 150px) / 5);
+  background: $primarycolor;
+  color: gray;
+    margin: 10px  15px;
+  }
+  img{
+   width: 150px;
+   height: 200px;
+  }
+  strong{
+    color: white;
+  }
 </style>
